e8e106afdca872dff764b553a91f635d97a7fe40
nphan2004 <nphan2004@gmail.com>
2021-07-30 18:58:57 -0400

    refactor

--- src/main/java/com/bd/chia/services/WalletTrackingRPC.java
+++ src/main/java/com/bd/chia/services/WalletTrackingRPC.java
@@ -4,37 +4,57 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Profile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.bd.chia.dto.WalletTransactions;
 import com.google.gson.Gson;
 
 @Component
-@Profile({"aws"})
 public class WalletTrackingRPC implements IWalletTrackingRPC {
-
+	private static final Logger log = LoggerFactory.getLogger(WalletTrackingRPC.class);
+	
+    @Value("${curl.path}")
+    private String curlExecutable;
+    
+    @Value("${curl.certificate}")
+    private String certificate;
+    
+    @Value("${curl.key}")
+    private String key;
+    
+    @Value("${curl.url}")
+    private String url;
+    
+    @Value("${curl.data}")
+    private String data;
+    
 	@Override
 	public WalletTransactions getWalletTransactions() throws Exception {
 		List<String> args = new ArrayList<String>();
-		args.add("/usr/bin/curl");
+		args.add(curlExecutable);
+		args.add("--verbose");
 		args.add("--insecure");
 		args.add("--cert");
-		args.add("/home/ubuntu/.chia/mainnet/config/ssl/wallet/private_wallet.crt");
+		args.add(certificate);
 		args.add("--key");
-		args.add("/home/ubuntu/.chia/mainnet/config/ssl/wallet/private_wallet.key");
+		args.add(key);
 		args.add("-d");
-		args.add("{'wallet_id': 1}");
+		args.add(data.trim());
 		args.add("-H");
 		args.add("Content-Type: application/json");
 		args.add("-X");
 		args.add("POST");
-		args.add("https://172.31.12.180:9256/get_transactions");				
-
+		args.add(url);				
+		
 		ProcessBuilder pb = new ProcessBuilder(args);
-
+		
 		Gson gson = new Gson();
+		
 		WalletTransactions wt = gson.fromJson(new InputStreamReader(pb.start().getInputStream()), WalletTransactions.class);
+
 		return wt;
 	}
 }
